972fbfddd78d1902275d5336ce90b63e521592ed
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-10-22 00:39:56 +0700

    add api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -11,7 +11,25 @@ module.exports = {
             
             res.status(200).json({data : voucher})
         } catch (err) {
-            res.status(500).json({message: err.message || `Terjadi Kesalahan pada server`})
+            res.status(500).json({message: err.message || `Internal Server Error`})
         }
-    }
+    },
+    detailPage : async(req, res) => {
+        try {
+            const { id } = req.params
+            const voucher = await Voucher.findOne({ _id : id})
+            .populate('category')
+            .populate('nominals')
+            .populate('user', '_id name phoneNumber')
+
+            if(!voucher){
+                return  res.status(404).json({message: "Voucher Game Tidak Ditemukan"})
+            }
+            
+            res.status(200).json({data : voucher})
+        } catch (err) {
+            res.status(500).json({message: err.message || `Internal Server Error`})
+        }
+    },
+
 }
