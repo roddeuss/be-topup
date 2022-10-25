aed4cffe78d7e642d06e6e521c5021b603411ade
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-10-25 15:34:59 +0700

    add api history detail

--- app/player/controller.js
+++ app/player/controller.js
@@ -161,6 +161,24 @@ module.exports = {
         } catch (err) {
             res.status(500).json({message: err.message || `Internal Server Error`})
         }
+    },
+
+    historyDetail : async (req, res) => {
+        try {
+            const { id } = req.params
+
+            const history = await Transaction.findOne({_id : id})
+
+            if(!history) return res.status(404).json({
+                message: "history tidak ditemukan"
+            })
+
+            res.status(200).json({
+                data: history
+            })
+        } catch (err) {
+            res.status(500).json({message: err.message || `Internal Server Error`})
+        }
     }
 
 }
