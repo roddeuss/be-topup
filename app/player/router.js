bc75c34574b7cc28414f9a03cf0a78d0a8f1be85
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-10-26 01:04:54 +0700

    add api dashboard

--- app/player/router.js
+++ app/player/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {landingPage, detailPage, category, checkout, history, historyDetail} = require('./controller')
+const {landingPage, detailPage, category, checkout, history, historyDetail, dashboard} = require('./controller')
 const {isLoginPlayer} = require('../middleware/auth')
 
 
@@ -10,5 +10,6 @@ router.get('/category', category);
 router.post('/checkout', isLoginPlayer ,checkout);
 router.get('/history', isLoginPlayer, history );
 router.get('/history/:id/detail', isLoginPlayer, historyDetail );
+router.get('/dashboard', isLoginPlayer, dashboard );
 
 module.exports = router;
