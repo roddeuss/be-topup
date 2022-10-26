1544e8b3bf7993a4f0e6873d7dd98dd2a10300c8
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-10-26 11:37:01 +0700

    add api edit profile

--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const {landingPage, detailPage, category, checkout, history, historyDetail, dashboard, profile} = require('./controller')
+const {landingPage, detailPage, category, checkout, history, historyDetail, dashboard, profile, editProfile} = require('./controller')
 const {isLoginPlayer} = require('../middleware/auth')
+const multer = require('multer');
+const os = require('os')
+
 
 
 router.get('/landingpage', landingPage);
@@ -12,5 +15,6 @@ router.get('/history', isLoginPlayer, history );
 router.get('/history/:id/detail', isLoginPlayer, historyDetail );
 router.get('/dashboard', isLoginPlayer, dashboard );
 router.get('/profile', isLoginPlayer, profile );
+router.put('/edit-profile', isLoginPlayer, multer({dest: os.tmpdir()}).single('image'), editProfile );
 
 module.exports = router;
